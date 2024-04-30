0917a25b582a63299080705f7cd6e7a239f8d9fb
QuesoParmesano <75513002+Queso-Parmesano@users.noreply.github.com>
2024-04-30 19:29:51 -0300

    Create Inserts.sql

---
+++ BaseDeDatos/Inserts.sql
@@ -0,0 +1,5 @@
+
+insert into productos (nombre, stock, color, precio) values ("Patito de Juguete", 23, "Amarillo", 25),
+("Bomba Nuclear, 25 Megatones", 32, "Gris y Blanco", 25000), 
+("Bombilla para mate", 230, "Plateado", 10), 
+("Placa Madre AM4 a320-m", 2, "Ninguno", 3000)
